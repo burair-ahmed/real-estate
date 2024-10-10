ac6200f8ca18eac5ac4df2763e1db42453a514ed
Burair Ahmed <burair.s3380@gmail.com>
2024-10-10 12:46:46 +0500

    "Updated MobileMenu component with new links and sub-menus, removed old links and reorganized menu structure."

--- src/components/header/elements/mobileMennu.jsx
+++ src/components/header/elements/mobileMennu.jsx
@@ -81,7 +81,16 @@ const MobileMenu = function ({ offCanVastoggleBtn, closeSideBar }) {
           <div className="ltn__utilize-menu">
             <ul>
               <li>
-                <Link href="#">Home</Link>
+                <Link href="/">HOME</Link>
+                {/* <span
+                  className="menu-expand"
+                  onClick={onClickHandler}
+                  aria-hidden="true"
+                ></span> */}
+              
+              </li>
+              <li>
+                <Link href="/about">ABOUT</Link>
                 <span
                   className="menu-expand"
                   onClick={onClickHandler}
@@ -89,76 +98,133 @@ const MobileMenu = function ({ offCanVastoggleBtn, closeSideBar }) {
                 ></span>
                 <ul className="sub-menu">
                   <li>
-                    <Link href="/home">Home Style 01</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-two">Home Style 02</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-three">Home Style 03</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-four">Home Style 04</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-five">Home Style 05</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-six">Home Style 06</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-seven">Home Style 07</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-eight">Home Style 08</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-nine">Home Style 09</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-ten">Home Style 10</Link>
-                  </li>
-                  <li>
-                    <Link href="/home/page-eleven">Home Style 11</Link>
+                    <Link href="/history">HISTORY</Link>
                   </li>
                 </ul>
               </li>
               <li>
-                <Link href="#">About</Link>
+                <Link href="">BUY</Link>
                 <span
                   className="menu-expand"
                   onClick={onClickHandler}
                   aria-hidden="true"
                 ></span>
                 <ul className="sub-menu">
-                  <li>
-                    <Link href="/about">About</Link>
-                  </li>
-                  <li>
-                    <Link href="/service">Services</Link>
-                  </li>
-
-                  <li>
-                    <Link href="/portfolio">Portfolio</Link>
-                  </li>
-                  <li>
-                    <Link href="/portfolio/page-two">Portfolio - 02</Link>
-                  </li>
-
-                  <li>
-                    <Link href="/team">Team</Link>
-                  </li>
-
-                  <li>
-                    <Link href="/faq">FAQ</Link>
-                  </li>
-                  <li>
-                    <Link href="/locations">Google Map Locations</Link>
-                  </li>
+                <li>
+              <Link href="/france/sale">FRANCE</Link>
+            </li>
+            <li>
+              <Link href="/united-states/sale">UNITED STATES</Link>
+            </li>
+            <li>
+              <Link href="/italy/sale">ITALY</Link>
+            </li>
+            <li>
+              <Link href="/spain/sale">SPAIN</Link>
+            </li>
+            <li>
+              <Link href="/switzerland/sale">SWITZERLAND</Link>
+            </li>
+            <li>
+              <Link href="/principality-of-monaco">PRINCIPALITY OF MONACO</Link>
+            </li>
+            <li>
+              <Link href="/uae">UNITED ARAB OF EMIRATES</Link>
+            </li>
+            <li>
+              <Link href="/portugal">PORTUGAL</Link>
+            </li>
+            <li>
+              <Link href="/andorra">ANDORRA</Link>
+            </li>
+            <li>
+              <Link href="/morroco">MORROCO</Link>
+            </li>
                 </ul>
               </li>
               <li>
-                <Link href="#">Property</Link>
+                <Link href="#">RENT</Link>
+                <span
+                  className="menu-expand"
+                  onClick={onClickHandler}
+                  aria-hidden="true"
+                ></span>
+                <ul className="sub-menu">
+                <li>
+              <Link href="#">HOLIDAYS </Link>
+              <span
+                  className="menu-expand"
+                  onClick={onClickHandler}
+                  aria-hidden="true"
+                ></span>
+              <ul>
+                <li>
+                  <Link href="/france/seasonal-rental/">FRANCE</Link>
+                </li>
+                <li>
+                  <Link href="/switzerland/seasonal-rental/">SWITZERLAND</Link>
+                </li>
+                <li>
+                  <Link href="/spain/seasonal-rental/">SPAIN</Link>
+                </li>
+                <li>
+                  <Link href="/portugal/seasonal-rental/">PORTUGAL</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link href="#">EVENTS</Link>
+              <span
+                  className="menu-expand"
+                  onClick={onClickHandler}
+                  aria-hidden="true"
+                ></span>
+              <ul>
+              <li>
+                  <Link href="/france/event/">FRANCE</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link href="#">LONG TERM</Link>
+              <span
+                  className="menu-expand"
+                  onClick={onClickHandler}
+                  aria-hidden="true"
+                ></span>
+              <ul>
+              <li>
+                  <Link href="/united-states/rental/">UNITED STATES</Link>
+                </li>
+                <li>
+                  <Link href="/switzerland/rental/">SWITZERLAND</Link>
+                </li>
+                <li>
+                  <Link href="/italy/rental/">ITALY</Link>
+                </li>
+                <li>
+                  <Link href="/spain/rental/">SPAIN</Link>
+                </li>
+                <li>
+                  <Link href="/uae/rental/">UNITED ARAB OF EMIRATES</Link>
+                </li>
+                <li>
+                  <Link href="/principality-of-monaco/rental/">PRINCIPALITY OF MONACO</Link>
+                </li>
+                <li>
+                  <Link href="/france/rental/">FRANCE</Link>
+                </li>
+                <li>
+                  <Link href="/andorra/rental/">ANDORRA</Link>
+                </li>
+              </ul>
+            </li>
+                </ul>
+
+                
+              </li>
+              <li>
+                <Link href="#">SERVICES</Link>
                 <span
                   className="menu-expand"
                   onClick={onClickHandler}
@@ -169,42 +235,10 @@ const MobileMenu = function ({ offCanVastoggleBtn, closeSideBar }) {
                     <Link href="/shop">Property</Link>
                   </li>
                   <li>
-                    <Link href="/shop/grid">Property Grid</Link>
-                  </li>
-                  <li>
-                    <Link href="/shop/list">Property List</Link>
-                  </li>
-                  <li>
-                    <Link href="/shop/left-sidebar">Property Left sidebar</Link>
-                  </li>
-                  <li>
-                    <Link href="/shop/right-sidebar">
-                      Property right sidebar
-                    </Link>
-                  </li>
-                  
-                  <li>
-                    <Link href="/cart">Cart</Link>
-                  </li>
-                  <li>
-                    <Link href="/wishlist">Wishlist</Link>
-                  </li>
-                  <li>
-                    <Link href="/checkout">Checkout</Link>
-                  </li>
-                  <li>
-                    <Link href="/order-tracking">Order Tracking</Link>
-                  </li>
-                  <li>
-                    <Link href="/my-account">My Account</Link>
-                  </li>
-                  <li>
-                    <Link href="/login">Sign in</Link>
-                  </li>
-                  <li>
-                    <Link href="/register">Register</Link>
+                    <Link href="/locations">Google Map Locations</Link>
                   </li>
                 </ul>
+                
               </li>
               {/* <li>
                 <Link href="#">News</Link>
