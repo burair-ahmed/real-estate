9e50b15b8e71fefa02be2f1ef96842167ee9f17f
Burair Ahmed <burair.s3380@gmail.com>
2024-10-24 12:07:47 +0500

    "Removed FaUser icon import, updated SlickArrowLeft and SlickArrowRight components, added responsive settings to disable arrows on mobile, and refactored HeroSectionStyleOne component's JSX structure."

--- src/components/hero/styleOne.jsx
+++ src/components/hero/styleOne.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Slider from "react-slick";
 import ModalVideo from "react-modal-video";
-import { FaPlay, FaHome, FaArrowRight, FaArrowLeft, FaUser } from "react-icons/fa";
+import { FaPlay, FaHome, FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 function HeroSectionStyleOne({ data }) {
   const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
@@ -18,6 +18,7 @@ function HeroSectionStyleOne({ data }) {
       <FaArrowLeft />
     </button>
   );
+
   const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
     <button
       {...props}
@@ -33,20 +34,29 @@ function HeroSectionStyleOne({ data }) {
     </button>
   );
 
+  // Add responsive settings to disable arrows on mobile
   const Herosettings = {
     dots: true,
     fade: true,
     cssEase: "linear",
-    arrow: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: <SlickArrowLeft />,
     nextArrow: <SlickArrowRight />,
+    responsive: [
+      {
+        breakpoint: 768, // Target screens below 768px width
+        settings: {
+          arrows: false, // Disable arrows on mobile
+        },
+      },
+    ],
   };
 
   const [isOpen, setOpen] = useState(false);
+  
   return (
     <>
       <ModalVideo
@@ -62,78 +72,71 @@ function HeroSectionStyleOne({ data }) {
           {...Herosettings}
           className="ltn__slide-one-active slick-slide-arrow-1 slick-slide-dots-1"
         >
-          {data.map((item, key) => {
-            return (
+          {data.map((item, key) => (
+            <div
+              className="ltn__slide-item ltn__slide-item-2 ltn__slide-item-3-normal ltn__slide-item-3 position-relative"
+              key={key}
+            >
               <div
-                className="ltn__slide-item ltn__slide-item-2 ltn__slide-item-3-normal ltn__slide-item-3 position-relative"
-                key={key}
+                className={`ltn__slide-item-inner ${
+                  item.variationLeft ? "text-right text-end" : ""
+                }`}
               >
-                <div
-                  className={`ltn__slide-item-inner ${
-                    item.variationLeft ? "text-right text-end" : ""
-                  }`}
-                >
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-lg-12 align-self-center">
-                        <div className="slide-item-info">
-                          <div className="slide-item-info-inner ltn__slide-animation">
-                            <h6 className="slide-sub-title animated">
-                              <span>
-                                <FaHome />
-                              </span>
-                              {item.subtitle}
-                            </h6>
-                            <h1 className="slide-title animated">
-                              {item.Title}
-                            </h1>
-                            <h1 className="slide-title animated">
-                              {item.Title2}
-                            </h1>
-                            <div className="slide-brief animated">
-                              <p>{item.Desc}</p>
-                            </div>
-                            <div className="btn-wrapper animated">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-12 align-self-center">
+                      <div className="slide-item-info">
+                        <div className="slide-item-info-inner ltn__slide-animation">
+                          <h6 className="slide-sub-title animated">
+                            <span>
+                              <FaHome />
+                            </span>
+                            {item.subtitle}
+                          </h6>
+                          <h1 className="slide-title animated">{item.Title}</h1>
+                          <h1 className="slide-title animated">{item.Title2}</h1>
+                          <div className="slide-brief animated">
+                            <p>{item.Desc}</p>
+                          </div>
+                          <div className="btn-wrapper animated">
+                            <Link
+                              href="/about"
+                              className="theme-btn-1 btn btn-effect-1"
+                            >
+                              {item.buttonText}
+                            </Link>
+
+                            {item.videoButton ? (
+                              <button
+                                onClick={() => setOpen(true)}
+                                className="ltn__video-play-btn bg-white"
+                              >
+                                <FaPlay className="icon-play  ltn__secondary-color" />
+                              </button>
+                            ) : (
                               <Link
                                 href="/about"
-                                className="theme-btn-1 btn btn-effect-1"
+                                className="btn btn-transparent btn-effect-3"
                               >
-                                {item.buttonText}
+                                {item.learnMoreButtonText}
                               </Link>
-
-                              {item.videoButton ? (
-                                <button
-                                  onClick={() => setOpen(true)}
-                                  className="ltn__video-play-btn bg-white"
-                                >
-                                  {/* <i className="icon-play  ltn__secondary-color"></i> */}
-                                  <FaPlay className="icon-play  ltn__secondary-color" />
-                                </button>
-                              ) : (
-                                <Link
-                                href="/about"
-                                  className="btn btn-transparent btn-effect-3"
-                                >
-                                  {item.learnMoreButtonText}
-                                </Link>
-                              )}
-                            </div>
+                            )}
                           </div>
                         </div>
-                        <div
-                          className={`slide-item-img ${
-                            item.variationLeft ? "slide-img-left" : ""
-                          }`}
-                        >
-                          <img src={item.heroimage} alt="#" />
-                        </div>
+                      </div>
+                      <div
+                        className={`slide-item-img ${
+                          item.variationLeft ? "slide-img-left" : ""
+                        }`}
+                      >
+                        <img src={item.heroimage} alt="#" />
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </Slider>
       </div>
     </>
